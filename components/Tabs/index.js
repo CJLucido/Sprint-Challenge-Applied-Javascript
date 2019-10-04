86ddf932a9c4e1a9984e460dcd49af118e530f57
CJ Lucido <c.jose.lucido@gmail.com>
2019-10-04 09:09:57 -0700

    completed tab challenge, the trick was to only process the values in the function and iterate over all the values in the then

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,45 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+const tabHolder = document.querySelector('.topics');
+
+axios
+    .get("https://lambda-times-backend.herokuapp.com/topics")
+    .then( response => {
+        console.log(response);
+        let info = [];
+        info = response.data;
+        console.log(info);
+        console.log(info.topics);
+        let info2 = info.topics; //couldn't put info.topics.forEach
+        info2.forEach(element => {
+            tabHolder.appendChild(createTab(element));
+            //the then has to attach the tab to the container
+        })
+     
+    })
+    .catch(err => {
+        console.log("You gotta knock a little harder.")
+    });
+
+
+
+
+function createTab(element){ //the function has to take the array VALUE
+    const tabee = document.createElement('div');
+
+    tabee.classList.add('tab');
+
+    //no structure needed!
+
+        tabee.textContent = element;
+    
+    
+return tabee
+
+}
+
+
+
+
+
